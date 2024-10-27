0b8c463fd055fbad2f9ca4e7cb1d7ecbe04270f8
Paweł Zawadzki <admin@machinacyfrowa.pl>
2024-10-27 21:36:28 +0100

    code comments

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
-
+//Autoloader od composera - odpowiada za automatyczne ładowanie klas zainstalowanych przez composera oraz naszych klas z katalogu src
+require_once('vendor/autoload.php'); 
+//Importujemy klasy z naszego namespace i namespace Steampixel
 use Steampixel\Route;
 use Machinacyfrowa\CornyApi\Joke;
 
-require_once('vendor/autoload.php'); 
-
+//Wczytujemy zmienne z pliku .env
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+//Tworzymy połączenie z bazą danych - używamy zmienne z pliku .env
 $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
-
-Route::add('/random', function() {
+//dodajemy najprostszą ścieżkę - pobieranie losowego żartu z bazy danych i zwracanie go w formacie JSON
+Route::add('/random', function() use($db){
     header('Content-Type: application/json');
     return json_encode(['joke' => Joke::getRandom()]);
 });
-
+//Zamykamy połączenie z bazą danych
 $db->close();
-
+//Uruchamiamy router
 Route::run($_ENV['REWRITE_BASE']);
 ?>
